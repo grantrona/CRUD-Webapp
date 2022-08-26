0c4f77ab2efdbc1457b0e72fd39ce7a55bb31bef
Ronald Grant <ronniegee198@gmail.com>
2022-08-26 03:01:54 -0400

    Fixed bug in which malloc did not initialise all of response builder struct's values to 0 - calloc now used to allocated memory

--- server.c
+++ server.c
@@ -7,19 +7,23 @@
 
 #define DEFAULT_STRLEN 30000
 #define SERVER_PORT  8080
+#define DEFAULT_NUM_HEADERS 10
+//#define SERVER_PORT  1
 
 const char *products[] = { "My Product 1", "My Product 2", "My Product 3", "My Product 4"};
 
 struct response_builder{
     char *http_ver;
     char *status;
-    char *headers[10]; // array of headers
+    char *headers[DEFAULT_NUM_HEADERS]; // array of headers
     char *body;
 } ;
 
 /* Define functions */
 void db_handle_client(int client_fd);
-char* construct_response(struct response_builder resp);
+void send_welcome(int client_fd, char* out_msg);
+void handle_get(int client_fd, char *query);
+char* construct_response(struct response_builder *resp);
 
 
 void error(const char *msg){
@@ -72,8 +76,6 @@ void db_handle_client(int client_fd)
 {
     char out_msg[DEFAULT_STRLEN];
     char in_msg[DEFAULT_STRLEN];
-    // length of the output message
-    u_long output_len = 0;
 
     if(client_fd < 0) {
         error("Failed to accept client connection\n");
@@ -82,29 +84,15 @@ void db_handle_client(int client_fd)
 
     /* Read request message from client and print the message*/
     memset(in_msg, 0, DEFAULT_STRLEN);
+    memset(out_msg, 0, DEFAULT_STRLEN);
     recv(client_fd, in_msg, DEFAULT_STRLEN, 0);
     printf("%s", in_msg);
 
     /* Send response to client */
     if (strstr(in_msg, "GET /") != NULL) {
-        // Generate response message
-        struct response_builder resp;
-        resp.http_ver = "HTTP/1.0";
-        resp.status = "200 OK";
-        char *content_type = "Content-Type: text/plain; charset=UTF-8";
-        char *conn = "Connection: close";
-        resp.headers[0] = content_type;
-        resp.headers[1] = conn;
-        resp.body = "Hello from Server";
+        char * query = in_msg + strlen("GET /");
 
-        // Concatenate components into a single char array
-        char *response = (char *) construct_response(resp);
-
-        output_len = strlen(response);
-        strcpy(out_msg, response);
-
-        // free dynamically allocated memory used to generate response
-        free(response);
+        send_welcome(client_fd, out_msg);
     }
     if (strstr(in_msg, "POST /") != NULL) {
         printf("DETECTED POST\n");
@@ -113,30 +101,56 @@ void db_handle_client(int client_fd)
         printf("DETECTED DELETE\n");
     }
 
-    // Send response to request
-    send(client_fd, out_msg, output_len, 0);
-
     //TODO CURRENTLY NON PERSISTENT!
     shutdown(client_fd,SHUT_RDWR);
 }
 
-char* construct_response(struct response_builder resp) {
+void send_welcome(int client_fd, char* out_msg) {
+    // Initialise memory for new response message to 0
+    struct response_builder* ptr = calloc(1, sizeof(struct response_builder));
+    if (ptr == NULL) {
+        error("Malloc error");
+    }
+
+    ptr->http_ver = "HTTP/1.0";
+    ptr->status = "200 OK";
+    char *content_type = "Content-Type: text/plain; charset=UTF-8";
+    char *conn = "Connection: close";
+    ptr->headers[0] = content_type;
+    ptr->headers[1] = conn;
+    ptr->body = "Hello from Server";
+
+    // Concatenate components into a single char array
+    char *response = (char *) construct_response(ptr);
+
+    u_long output_len = strlen(response);
+    strcpy(out_msg, response);
+
+    // Send response to request
+    send(client_fd, out_msg, output_len, 0);
+
+    // free dynamically allocated memory used to generate response
+    free(response);
+    free(ptr);
+}
+
+char* construct_response(struct response_builder *resp) {
     char *message;
-    message = malloc(strlen(resp.http_ver) + 2);
-    strcpy(message, resp.http_ver);
+    message = malloc(strlen(resp->http_ver) + 2);
+    strcpy(message, resp->http_ver);
     strcat(message, " ");
     strcat(message, "\0");
 
-    char *concat_status = realloc(message, strlen(resp.status) + strlen(message) + 2);
-    strcat(concat_status, resp.status);
+    char *concat_status = realloc(message, strlen(resp->status) + strlen(message) + 2);
+    strcat(concat_status, resp->status);
     strcat(concat_status, "\n");
     strcat(concat_status, "\0");
 
     char* all_headers = malloc(0);
-    int num_headers = sizeof(resp.headers)/sizeof(resp.headers[0]);
+    int num_headers = sizeof(resp->headers)/sizeof(resp->headers[0]);
     for (int i = 0; i < num_headers; i++) {
-        if (resp.headers[i] == NULL) { break; }
-        char *header = resp.headers[i];
+        if (resp->headers[i] == NULL) { break; }
+        char *header = resp->headers[i];
         all_headers = realloc(all_headers, strlen(header) + strlen(all_headers) + 2);
         strcat(all_headers, header);
         strcat(all_headers, "\n");
@@ -148,8 +162,8 @@ char* construct_response(struct response_builder resp) {
     strcat(concat_headers, "\n");
     strcat(concat_headers, "\0");
 
-    char *concat_body = realloc(concat_headers, strlen(resp.body) + strlen(concat_headers) + 2);
-    strcat(concat_body, resp.body);
+    char *concat_body = realloc(concat_headers, strlen(resp->body) + strlen(concat_headers) + 2);
+    strcat(concat_body, resp->body);
     strcat(concat_body, "\n");
     strcat(concat_body, "\0");
 
