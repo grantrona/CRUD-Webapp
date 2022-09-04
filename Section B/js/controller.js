3329f0f2952b68bbc1734dc33382f1a3e8354291
Ronald Grant <ronniegee198@gmail.com>
2022-09-04 08:30:32 -0400

    Updated node.js adding postgres support

--- Section B/js/controller.js
+++ Section B/js/controller.js
@@ -1,4 +1,13 @@
 window.addEventListener("load", init);
+const {Client} = require('pg');
+
+const client = new Client({
+    host: "ec2-3-224-8-189.compute-1.amazonaws.com",
+    user: "qfszfxgiijpwtf",
+    port: "5432",
+    password: "ef957c66f851132c19567be8fb532131809b8cd265d0e3ceb99ec481a2097904",
+    database: "d4vi9mo6kqq81k",
+});
 
 function init() {
 
@@ -309,5 +318,12 @@ function saveToServer() {
 }
 
 function loadFromServer() {
-    alert("TODO load");
+    client.connect();
+    //Client promise TODO
+    client
+        .query('SELECT * from ITEMS')
+        .then(res => console.log("FETCHED!\n" + res))
+        .catch(e => console.error(e.stack));
+
+    client.end();
 }
